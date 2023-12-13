0e39814bc1fec9974be9396d1aa6f7d2cfe13e12
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-12-13 21:38:39 +0200

    add: daysCalculator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './index.html';
 import './index.scss';
 import timeConverter from './modules/timeConverter';
+import dayCalculator from './modules/daysCalculator';
 
 window.addEventListener('DOMContentLoaded', () => {
 timeConverter();
+dayCalculator();
+
 });
